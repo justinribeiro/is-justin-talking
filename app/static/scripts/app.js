9e21f4d36d1de28fecca2caf1748784467484794
Justin Ribeiro <justin@justinribeiro.com>
2016-02-18 18:32:37 -0800

    WIP

---
+++ app/static/scripts/app.js
@@ -0,0 +1,140 @@
+// The base ref were our subscriptions will be pushed
+var firebaseRoot = 'https://is-justin-talking.firebaseio.com/subscriptions';
+
+// This is replaced when we have a subscription
+var existingSubscriptionId = 0;
+
+// DOM DOM DOM DOM
+var pushMessaging = document.querySelector('platinum-push-messaging');
+var togglePush = document.getElementById('togglePush');
+var noisesOn = document.getElementById('noisesOn');
+var noisesOff = document.getElementById('noisesOff');
+
+// Do you really need the polyfill?
+var webComponentsSupported = ('registerElement' in document
+    && 'import' in document.createElement('link')
+    && 'content' in document.createElement('template'));
+
+if (!webComponentsSupported) {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = '/bower_components/webcomponentsjs/webcomponents-lite.min.js';
+  document.head.appendChild(script);
+
+  window.addEventListener('WebComponentsReady', function(e) {
+    console.log('FALLBACK: WebComponentsReady() fired and components ready.');
+    appInit();
+  });
+
+} else {
+  window.Polymer = window.Polymer || {dom: 'shadow'};
+
+  var link = document.querySelector('#bundle');
+
+  var onImportLoaded = function() {
+    console.log('NATIVE: Elements are upgraded!');
+    appInit();
+  };
+
+  // 5. Go if the async Import loaded quickly. Otherwise wait for it.
+  // crbug.com/504944 - readyState never goes to complete until Chrome 46.
+  // crbug.com/505279 - Resource Timing API is not available until Chrome 46.
+  if (link.import && link.import.readyState === 'complete') {
+    onImportLoaded();
+  } else {
+    link.addEventListener('load', onImportLoaded);
+  }
+}
+
+function appInit() {
+
+  // Go get the schedule
+  ajax('schedule.json', function(err, data) {
+    document.querySelector('are-you-talking').schedule = data;
+  });
+
+  // Setup base ref for Firebase
+  var firebaseRef = new Firebase(firebaseRoot);
+
+  // Check the current state
+  togglePush.checked = pushMessaging.enabled;
+
+  togglePush.addEventListener('change', function() {
+    if (togglePush.checked) {
+      pushMessaging.enable();
+    } else {
+      pushMessaging.disable();
+    }
+  });
+
+  pushMessaging.addEventListener('subscription-changed', function(event) {
+    // GCM always needs the subscriptionId passed separately. Note that as of M45,
+    // the subscriptionId and the endpoint have merged.
+    var subscriptionId = pushMessaging.subscription ? pushMessaging.subscription.subscriptionId : undefined;
+    
+    if (pushMessaging.subscription && !pushMessaging.subscription.subscriptionId) {
+      var endpointSections = pushMessaging.subscription.endpoint.split('/');
+      subscriptionId = endpointSections[endpointSections.length - 1];
+    } 
+
+    console.log('PushMessaging subscription changed, subId: %s', subscriptionId);
+
+    if (subscriptionId) {
+      existingSubscriptionId = subscriptionId;
+    }
+
+  });
+
+  pushMessaging.addEventListener('enabled-changed', function(event) {
+
+    console.log('PushMessaging state changed: %s', pushMessaging.enabled);
+
+    togglePush.checked = pushMessaging.enabled;
+
+    var subscriptionRef = firebaseRef.child(existingSubscriptionId);
+    if (pushMessaging.enabled) {
+      subscriptionRef.set({
+        subscriptionId: existingSubscriptionId,
+        timestamp: Firebase.ServerValue.TIMESTAMP
+      });
+    } else {
+      subscriptionRef.remove();
+      console.log('removed record');
+    }
+
+  });
+
+}
+
+/**
+ * [ajax description]
+ * @param  {[type]}   url [description]
+ * @param  {Function} cb  [description]
+ * @return {[type]}       [description]
+ */
+function ajax(url, cb) {
+  // Native fetch doesn't seem to reuse connections with h2 push
+  // so using XHR instead
+  var req = new XMLHttpRequest();
+  req.addEventListener('load', function() {
+    var data;
+    var error;
+    try {
+      data = JSON.parse(this.responseText);
+    } catch (err) {
+      error = err;
+    }
+    cb(error, data);
+  });
+  req.open('GET', '/data/' + url);
+  req.send();
+}
+
+// GA
+//(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+//
+//ga('create', '', 'auto'); 
+//ga('require', 'linkid', 'linkid.js'); 
---
+++ gulpfile.js
@@ -0,0 +1,157 @@
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+'use strict';
+
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var vulcanize = require('gulp-vulcanize');
+var merge = require('merge-stream');
+var glob = require('glob');
+var crypto = require('crypto');
+var path = require('path');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var minifyHTML = require('gulp-minify-html');
+var minifyInline = require('gulp-minify-inline');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var jpegtran = require('imagemin-jpegtran');
+
+var packageJson = require('./package.json');
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('copy', ['clean'], function() {
+  var app = gulp.src([
+    'app/static/**/*',
+    '!app/static/images/**/*',
+    '!app/static/index.html',
+    '!app/static/scripts/**/*',
+    '!app/static/elements/**/*',
+    '!app/static/bower_components/**/*'
+  ])
+  .pipe(gulp.dest('dist/static'));
+
+  var bower = gulp.src([
+    'app/static/bower_components/{webcomponentsjs,platinum-sw,sw-toolbox,promise-polyfill,firebase}/**/*'
+  ])
+  .pipe(gulp.dest('dist/static/bower_components'));
+
+  return merge(app, bower);
+});
+
+gulp.task('html:js:dev', ['clean'], function() {
+  return gulp.src('app/static/index.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify({mangle: false, compress: {
+         drop_console: false
+    }})))
+    .pipe(gulp.dest('tmp/static'))
+    .pipe(gulp.dest('dist/static'));
+});
+
+gulp.task('html:js', ['clean'], function() {
+  return gulp.src('app/static/index.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify({compress: {
+         drop_console: true
+    }})))
+    .pipe(gulp.dest('tmp/static'))
+    .pipe(gulp.dest('dist/static'));
+});
+
+gulp.task('images:min', ['clean'], function() {
+  return gulp.src('app/static/images/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant(), jpegtran()]
+    }))
+    .pipe(gulp.dest('dist/static/images'));
+});
+
+gulp.task('html:compress', ['html:js'], function() {
+  return gulp.src('tmp/static/index.html')
+    .pipe(useref())
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist/static'));
+});
+
+gulp.task('vulcanize', ['copy'], function() {
+  return gulp.src('app/static/elements/elements.html')
+    .pipe(vulcanize({
+      stripComments: true,
+      inlineCss: true,
+      inlineScripts: true,
+      excludes: [path.resolve('./app/static/bower_components/firebase/firebase.js')]
+    }))
+    .pipe(minifyInline())
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist/static/elements'));
+});
+
+gulp.task('cache-config', ['copy', 'html:js', 'html:compress', 'images:min', 'vulcanize'], function(callback) {
+  var dir = 'dist/static';
+  var config = {
+    cacheId: packageJson.name,
+    disabled: false
+  };
+
+  glob('{data,scripts,images}/**/*.*', {cwd: dir}, function(error, files) {
+    if (error) {
+      callback(error);
+    } else {
+      files.push(
+        '/',
+        'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+        'script/app.js',
+        'elements/elements.html'
+      );
+      config.precache = files;
+
+      var md5 = crypto.createHash('md5');
+      md5.update(JSON.stringify(config.precache));
+      config.precacheFingerprint = md5.digest('hex');
+
+      var configPath = path.join(dir, 'cache-config.json');
+      fs.writeFile(configPath, JSON.stringify(config), callback);
+    }
+  });
+});
+
+gulp.task('watch', ['dev'], function() {
+  gulp.watch([
+    'app/static/index.html',
+    'app/static/favicon.ico',
+    'app/static/{scripts,elements,styles}/**/*'
+  ]);
+});
+
+gulp.task('deploy:gae', ['default'], function() {
+  exec('appcfg.py update app.yaml', function(err, stdout, stderr) {
+    if (err) {
+      console.log('Child process exited with error code', err.code);
+      return;
+    }
+    console.log(stdout);
+    console.log(stderr);
+  });
+});
+
+gulp.task('dev', ['clean', 'html:js:dev', 'images:min', 'vulcanize']);
+
+gulp.task('default', ['clean', 'html:js', 'html:compress', 'images:min', 'vulcanize', 'cache-config']);
+
+gulp.task('deploy', ['default', 'deploy:gae']);
